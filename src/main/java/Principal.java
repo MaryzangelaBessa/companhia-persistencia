c1b36fa5da051276bed412aa6e5d729ad36a2bfb
MaryzangelaBessa <adm.maryzangelabessa@gmail.com>
2019-10-30 23:59:35 -0300

    Importação do lombok e implementação do 1º teste de inserção

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,5 +1,26 @@
+import daos.DepartamentoDAO;
+import daos.jpa.DepartamentoJPADAO;
+import models.Departamento;
+
+import javax.persistence.PersistenceException;
+
 public class Principal {
     public static void main(String[] args) {
-        
+        inserirDepartamento();
+    }
+
+    public static void inserirDepartamento(){
+        DepartamentoDAO dDAO = new DepartamentoJPADAO();
+
+        try {
+            dDAO.beginTransaction();
+            dDAO.save(new Departamento("Computação"));
+            dDAO.commit();
+        }catch (IllegalStateException | PersistenceException e){
+            dDAO.rollback();
+            e.printStackTrace();
+        }finally {
+            dDAO.close();
+        }
     }
 }
