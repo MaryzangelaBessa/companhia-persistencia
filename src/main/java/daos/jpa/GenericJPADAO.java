b2918274ae63510ed430dfd804b176902efe9c0f
MaryzangelaBessa <adm.maryzangelabessa@gmail.com>
2019-10-30 23:16:51 -0300

    Implementação dos metodos em GenericJPADAO

--- src/main/java/daos/jpa/GenericJPADAO.java
+++ src/main/java/daos/jpa/GenericJPADAO.java
@@ -1,7 +1,9 @@
 package daos.jpa;
 
 import daos.GenericDAO;
+import jpa.JPAUtil;
 
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 public class GenericJPADAO<T> implements GenericDAO<T> {
@@ -13,30 +15,32 @@ public class GenericJPADAO<T> implements GenericDAO<T> {
     }
 
     public void save(T entity) {
-
+        JPAUtil.getEntityManager().merge(entity);
     }
 
     public void delete(T entity) {
-
+        JPAUtil.getEntityManager().remove(JPAUtil.getEntityManager().merge(entity));
     }
 
     public List<T> findAll() {
-        return null;
+        CriteriaQuery<T> cq = JPAUtil.getEntityManager().getCriteriaBuilder().createQuery(persistenceClass);
+        cq.from(persistenceClass);
+        return JPAUtil.getEntityManager().createQuery(cq).getResultList();
     }
 
     public void beginTransaction() {
-
+        JPAUtil.beginTransaction();
     }
 
     public void commit() {
-
+        JPAUtil.commit();
     }
 
     public void rollback() {
-
+        JPAUtil.rollback();
     }
 
     public void close() {
-
+        JPAUtil.closeEntityManager();
     }
 }
