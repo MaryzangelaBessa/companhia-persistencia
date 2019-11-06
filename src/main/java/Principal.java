e21fe752887a47b195b9c713e85802ba3ba4dfe1
MaryzangelaBessa <adm.maryzangelabessa@gmail.com>
2019-11-06 17:11:57 -0300

    Menu Principal feito

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,27 +1,64 @@
 import daos.DepartamentoDAO;
 import daos.jpa.DepartamentoJPADAO;
+import menus.MenuDepartamentos;
+import menus.MenuFuncionarios;
+import menus.MenuProjetos;
 import models.Departamento;
 
 import javax.persistence.PersistenceException;
+import java.util.Scanner;
 
 public class Principal {
+
+    private static Scanner scanner;
+
     public static void main(String[] args) {
-        inserirDepartamento();
-    }
 
-    public static void inserirDepartamento(){
-        DepartamentoDAO dDAO = new DepartamentoJPADAO();
+        boolean run = true;
+        scanner = new Scanner(System.in);
 
-        try {
-            dDAO.beginTransaction();
-            //dDAO.save(new Departamento("Computação"));
-            dDAO.save(new Departamento("RH"));
-            dDAO.commit();
-        }catch (IllegalStateException | PersistenceException e){
-            dDAO.rollback();
-            e.printStackTrace();
-        }finally {
-            dDAO.close();
+        while (run) {
+            System.out.println("----------MENU----------");
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Menu Departamentos");
+            System.out.println("2 - Menu Projetos");
+            System.out.println("3 - Menu Funcionários");
+            System.out.println("0 - Sair");
+            System.out.println("----------------------------");
+            System.out.print(">>> ");
+
+            int menu = -1;
+
+            try {
+                menu = Integer.parseInt(scanner.nextLine());
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+            switch (menu) {
+                case 1:
+                    new MenuDepartamentos().menuDepartamento();
+                    break;
+
+                case 2:
+                    new MenuProjetos().menuProjetos();
+                    System.out.println("Menu Projetos");
+                    break;
+
+                case 3:
+                    new MenuFuncionarios().menuFuncionarios();
+                    System.out.println("Menu Funcionários");
+                    break;
+
+                case 0:
+                    run = false;
+                    System.out.println("GoodBye!");
+                    return;
+
+                default:
+                    System.out.println("Escolha Invalida!");
+                    break;
+            }
         }
     }
 }
